f457acb0f1e10fd9c93efbb3dff233db9d87ceac
andersonms92 <anderson.matos.santos@everis.com>
2021-08-21 18:05:13 -0300

    Iniciando conexão com api

---
+++ app/src/main/java/com/example/apicartoes/data/model/CartoesModel.kt
@@ -0,0 +1,12 @@
+package com.example.apicartoes.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class CartoesModel(
+    @SerializedName("id") val id:String?,
+    @SerializedName("username") val name:String,
+    val password: String,
+    @SerializedName("cardnumber") val CardNumber:String,
+    @SerializedName("code") val code:String,
+    @SerializedName("date") val date:String
+)
---
+++ app/src/main/java/com/example/apicartoes/data/model/MockApi.kt
@@ -0,0 +1,10 @@
+package com.example.apicartoes.data.model
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface MockApi {
+
+    @GET("/api/v1/login")
+    fun getDataDemandaShadowApi(): Call<List<CartoesModel>>
+}
